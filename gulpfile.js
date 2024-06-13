24392dc86571ce521f45a10746b5803a1be891b9
Esther Elbaz <esther.e@ouieqare.com>
2024-06-13 12:57:46 +0300

    ads.txt

--- gulpfile.js
+++ gulpfile.js
@@ -206,7 +206,10 @@ gulp.task('generate-sitemap', function () {
     }))
     .pipe(gulp.dest(path.build.dir));
 });
-
+gulp.task('copy-ads', function() {
+  return gulp.src('path/to/theme/ads.txt')  // Assurez-vous que le chemin est correct
+      .pipe(gulp.dest('path/to/dist/theme')); // Modifiez le chemin de destination selon votre structure
+});
 
 
 
@@ -244,6 +247,7 @@ gulp.task(
     "plugins:build",
     "others:build",
     "generate-sitemap",
+    "copy-ads",
     "start",
     gulp.parallel("watch:build", function () {
       bs.init({
